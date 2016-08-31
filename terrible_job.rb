44abbdf55eab634a20495c5774f90363cc334c89
Jaroslaw Swierad <jaroslaw.swierad@sanoma.com>
2016-08-31 14:21:39 +0200

    new version

--- terrible_job.rb
+++ terrible_job.rb
@@ -1,7 +1,7 @@
 class TerribleJob < Formula
   homepage "https://terrible.sanomaservices.nl"
   url "https://terrible.sanomaservices.nl/html/terrible_job-mac.tar.gz"
-  version "1.0.1"
+  version "1.0.2"
   sha256 "52b633315ce2f4d9d8249e83ce83798c9517e72269d9d3896a6699cef97e61e4"
 
   def install
