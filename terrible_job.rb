0c4824a1c114c377580e8360d06659c1c3130a6d
Jaroslaw Swierad <jaroslaw.swierad@sanoma.com>
2016-08-31 14:23:05 +0200

    correct sha

--- terrible_job.rb
+++ terrible_job.rb
@@ -2,7 +2,7 @@ class TerribleJob < Formula
   homepage "https://terrible.sanomaservices.nl"
   url "https://terrible.sanomaservices.nl/html/terrible_job-mac.tar.gz"
   version "1.0.2"
-  sha256 "52b633315ce2f4d9d8249e83ce83798c9517e72269d9d3896a6699cef97e61e4"
+  sha256 "623b864c204cbc18b666a8d3e6f2c83f5b0ae43a0c692eaa9b8e2616c068ac59"
 
   def install
       bin.install "terrible_job"
