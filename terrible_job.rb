54b02de949174d802635165fd604e3faa346a178
José Moreira <zemanel@zemanel.eu>
2016-09-26 17:00:31 +0200

    Release 1.0.3

--- terrible_job.rb
+++ terrible_job.rb
@@ -1,8 +1,8 @@
 class TerribleJob < Formula
   homepage "https://terrible.sanomaservices.nl"
   url "https://terrible.sanomaservices.nl/html/terrible_job-mac.tar.gz"
-  version "1.0.2"
-  sha256 "623b864c204cbc18b666a8d3e6f2c83f5b0ae43a0c692eaa9b8e2616c068ac59"
+  version "1.0.3"
+  sha256 "7f9f02d32dccd8342685388c71d8670258aeb101f8dd5f6bd4268950030ab505"
 
   def install
       bin.install "terrible_job"
