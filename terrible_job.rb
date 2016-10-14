93c5ccbaf21048ab77185dfead3cafe5516f221e
xaniasd <xanias@gmail.com>
2016-10-14 11:05:51 +0200

    Update terrible_job.rb
    update terrible_job v1.0.5

--- terrible_job.rb
+++ terrible_job.rb
@@ -1,8 +1,8 @@
 class TerribleJob < Formula
   homepage "https://terrible.sanomaservices.nl"
   url "https://terrible.sanomaservices.nl/html/terrible_job-mac.tar.gz"
-  version "1.0.3"
-  sha256 "7f9f02d32dccd8342685388c71d8670258aeb101f8dd5f6bd4268950030ab505"
+  version "1.0.5"
+  sha256 "0d8bf717f45fb3e557d9720609651d75a813b81fcea6db57f454e5a320090f6c"
 
   def install
       bin.install "terrible_job"
