10019d3800c51d67f1f6abf0dfe253efef2a45d7
Marco Lebbink <marco.lebbink@sanoma.com>
2016-12-23 13:25:22 +0100

    Bump to version v1.3.0

--- terrible_job.rb
+++ terrible_job.rb
@@ -1,8 +1,8 @@
 class TerribleJob < Formula
   homepage "https://terrible.sanomaservices.nl"
   url "https://terrible.sanomaservices.nl/html/terrible_job-mac.tar.gz"
-  version "v1.2.1"
-  sha256 "d7ad68354d1ab4c9e53061cce41951cbeb572bbc37cefe3df71918509c33d8b8"
+  version "v1.3.0"
+  sha256 "a363ae0bf949e93a0059ca98fd15855cfa6073767cdc671ff52c83ae46ea276f"
 
   def install
       bin.install "terrible_job"
