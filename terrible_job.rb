f7281a9b57ac8e3d163fa6b8915b4c86d6255ac2
Terrible Bamboo Builder <bamboo@sanoma.com>
2017-04-05 13:02:52 +0000

    Bump to version v1.8.24

--- terrible_job.rb
+++ terrible_job.rb
@@ -1,8 +1,8 @@
 class TerribleJob < Formula
   homepage "https://terrible.sanomaservices.nl"
   url "https://terrible.sanomaservices.nl/html/terrible_job-mac.tar.gz"
-  version "1.8.23"
-  sha256 "aadf7c1d9c5830050fd9dc9c40474c85eabb1bb8a71ddacfcff7f4f7f5bf163d"
+  version "1.8.24"
+  sha256 "31c74598609d049afbaa3b3922b64a9158b33ea5a5b3f74ad9c52694264df46d"
 
   def install
       bin.install "terrible_job"
